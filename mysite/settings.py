045c6ad1b781788c8e1236fc25f01a9701cdefb7
g23tk007 <g23tk007@yamanashi.ac.jp>
2023-12-12 10:39:57 +0900

    12_12

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,12 +125,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 """STATICFILES_DIRS = (
     [
         os.path.join(BASE_DIR, "QAmuseum/static"), 
